5ffc0e81f084128c79fd469c3c5725e81f2588af
ganeshi4u <ganeshvarmai4u@gmail.com>
2020-08-03 02:02:40 +0530

    download: Add some basic transition effects
    Change-Id: I4c8eff586cb3181f4930686bde5d47fb0d9d90f0

--- js/download.js
+++ js/download.js
@@ -6,13 +6,22 @@ $(document).ready(function() {
     if (prevSelectedDevice == null) {
         $('#device-content').load("empty.html");
     } else {
-        $('#device-content').load("device.php", 'device=' + prevSelectedDevice);
+        $('#device-content').addClass("scale-transition scale-out");
+        $('#device-content').load("device.php", 'device=' + prevSelectedDevice, function() {
+            $('#device-content').removeClass("scale-transition scale-out");
+            $('#device-content').addClass("scale-transition");
+        });
     }
 
     $('body').on('click', '#deviceLabel', function() {
+        $('#device-content').addClass("scale-transition scale-out");
         selectedDevice = $(this).text();
         localStorage.setItem("device", selectedDevice);
-        $('#device-content').load("device.php", 'device=' + selectedDevice);
+        $('#device-content').load("device.php", 'device=' + selectedDevice, function() {
+            $('#device-content').removeClass("scale-transition scale-out");
+            $('#device-content').addClass("scale-transition");
+            $(window).scrollTop(0);
+        });
     });
 
     $('body').on('click', '#select-device', function() {
